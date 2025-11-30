6725df0be45b55591c5ccb05f91e425e94a99458
rxhemanth4533 <123084965+rxhemanth4533@users.noreply.github.com>
2025-11-30 16:51:08 +0530

    Update program-1.cpp

--- program-1.cpp
+++ program-1.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+using namespace std;
 
+// simple calculator class
+class Calculator {
+public:
+    double a;
+    double b;
+
+    // constructor to store numbers
+    Calculator(double x, double y) {
+        a = x;
+        b = y;
+    }
+
+    // function which does operation based on the symbol
+    double calc(char op) {
+        double ans = 0;
+        if (op == '+') {
+            ans = a + b;
+        } else if (op == '-') {
+            ans = a - b;
+        } else if (op == '*') {
+            ans = a * b;
+        } else if (op == '/') {
+            // small check so that b is not zero
+            if (b != 0)
+                ans = a / b;
+            else {
+                cout << "Cannot divide by zero
+";
+            }
+        } else {
+            cout << "Unknown operation
+";
+        }
+        return ans;
+    }
+};
+
+int main() {
+    double x, y;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> op;
+
+    Calculator c(x, y);              // object of class
+    double result = c.calc(op);      // call function
+
+    cout << "Result = " << result << endl;
+    return 0;
+}
